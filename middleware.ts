339f05376dd18b196b889de3c8b099c04e49dc52
lyra-planet <1170158548@qq.com>
2022-10-28 23:43:22 +0800

    fix

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import {NextResponse} from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export const middleware = async (req:NextRequest ) => {
-    console.log(req)
+    console.log(process.env.JWT_SECRET)
     const token = await getToken({req,secret:process.env.JWT_SECRET});
    
     const {pathname,origin} = req.nextUrl
