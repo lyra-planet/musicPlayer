91998972982d7cc596d47e896134148d4e28f462
lyra-planet <1170158548@qq.com>
2022-10-28 23:35:19 +0800

    fix

--- middleware.ts
+++ middleware.ts
@@ -6,15 +6,16 @@ export const middleware = async (req:NextRequest ) => {
     const token = await getToken({req,secret:process.env.JWT_SECRET});
    
     const {pathname,origin} = req.nextUrl
+    console.log(token)
     if(pathname.includes('/api/auth')||token){
- 
+        
         return NextResponse.next()
     }
-    // const url = req.nextUrl.clone()
-    // url.pathname = '/login'
-    // if(!token&&pathname!=='/login'){
-    //     return NextResponse.redirect(url)
-    // }
+    const url = req.nextUrl.clone()
+    url.pathname = '/login'
+    if(!token&&pathname!=='/login'){
+        return NextResponse.redirect(url)
+    }
 }
 export const config={
     matcher:['/api/auth','/login','/']
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil'
+import Head from 'next/head'
 
 //@ts-ignore
 function MyApp({ Component, pageProps:{session,...pageProps} }: AppProps) {
   return  <SessionProvider session={session}>
+            <Head>
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+            </Head>
             <RecoilRoot>
             <Component {...pageProps} />
             </RecoilRoot>
